85580409763c3faf28adcf8c6520940ddd19848c
amond2 <8hyojae@gmail.com>
2025-03-27 19:28:01 +0900

    feat: enemyData 추가

--- Assets/2.Scripts/Enemy/EnemyData.cs
+++ Assets/2.Scripts/Enemy/EnemyData.cs
@@ -2,9 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyData : MonoBehaviour
+[CreateAssetMenu(fileName = "NewEnemyData", menuName = "Enemy Data")]
+public class EnemyData : ScriptableObject
 {
     public string enemyName;
     public int maxHealth;
     public int enemyCount;
+
+    private void Awake()
+    {
+        enemyName = this.name;
+        maxHealth = this.maxHealth;
+        enemyCount = this.enemyCount;
+    }
 }
