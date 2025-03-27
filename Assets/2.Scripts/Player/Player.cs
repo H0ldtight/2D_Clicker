20748dc4b48e9b0c394048c02991ea442beaa5dc
hannah896 <phn2260@naver.com>
2025-03-27 21:03:56 +0900

    Feat: “업그레이드 메서드 추가”
    플레이어의 스텟 강화를 위한 업그레이드 메서드 완성.

--- Assets/2.Scripts/Player/Player.cs
+++ Assets/2.Scripts/Player/Player.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.U2D.Aseprite;
 using UnityEngine;
+using UnityEngine.AI;
 
 //강화할 수 있는 옵션
 public enum UpgradeType
@@ -11,49 +13,54 @@ public enum UpgradeType
     PlusGold
 }
 
-//업그레이드 요소
+//업그레이드 옵션 
 [Serializable]
-public struct Upgrade
+public class UpgradeOption
 {
+    //강화 옵션명
     public UpgradeType UpgradeType;
-    public string level;
-    public float requireGold;
+    //레벨업 시 증가량
+    public float value;
+    //현재 레벨
+    public int level;
+    //요구골드
+    public int requireGold;
 }
 
 public class Player
 {
     //능력치
-    public StatData stat;
-    public List<Upgrade> upgradeOptions;
+    public StatData statData;
+    //업그레이드 가능한 옵션 딕셔너리<옵션, 증가시켜줄 스텟>
+    public Dictionary<UpgradeOption, StatType> upgradeOptions;
 
     //재화
     public int point;
     public int gold;
     
-    public Player()
+    //생성자
+    public Player(StatData statData)
     {
         point = 0;
         gold = 0;
+        this.statData = statData;
+        statData.stats.Add(StatType.AttackPower, 0);
     }
 
-    //public void Upgrade(UpgradeType type)
-    //{
-    //    if (gold < RequireGold)
-    //    switch(type)
-    //    {
-    //        case UpgradeType.AutoAttack:
-    //            {
-    //                stat.stats[] += 
-    //                break;
-    //            }
-    //        case UpgradeType.Critical:
-    //            {
-    //                break;
-    //            }
-    //        case UpgradeType.PlusGold:
-    //            {
-    //                break;
-    //            }
-    //    }
-    //}
+    //업그레이드하기
+    public void Upgrade(UpgradeOption upgrade)
+    {
+        //보유 골드 모자랄 시 업그레이드 불가
+        if (gold < upgrade.requireGold)
+        {
+            // TODO: UI 나중에 연결하기
+            return;
+        }
+
+        StatType stat = upgradeOptions[upgrade];
+
+        upgrade.level++;
+        this.statData.stats[stat] += upgrade.level * upgrade.value;
+        upgrade.requireGold = upgrade.requireGold * 2;
+    }
 }
--- Assets/2.Scripts/Player/ScriptableObject/StatData.cs
+++ Assets/2.Scripts/Player/ScriptableObject/StatData.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 //능력치 관련
 public enum StatType
 {
-    AttackPerSecond,
-    CriticalPercent,
-    ExtraGold,
-    AttackPower
+    AttackPerSecond,    //초당공격
+    Criticaldamage,     //크리티컬 데미지
+    ExtraGold,          //증가한 골드획득량
+    AttackPower         //공격력
 }
 
 [CreateAssetMenu(fileName = "NewStatBase", menuName ="Stats")]
